b6a93d4e8e3296d1e0e00229d4ce3d9a7692ed49
Vladislav Antonyuk <vlad.antonyuk@outlook.com>
2021-11-06 00:12:39 +0200

    Set sqlite provider

--- KanbanBoard/Platforms/iOS/AppDelegate.cs
+++ KanbanBoard/Platforms/iOS/AppDelegate.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using SQLitePCL;
 
 namespace KanbanBoard
 {
     [Register(nameof(AppDelegate))]
 	public class AppDelegate : MauiUIApplicationDelegate
 	{
-		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+		protected override MauiApp CreateMauiApp()
+		{
+			raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
+			return MauiProgram.CreateMauiApp();
+		}
 	}
 }
